a9195f601ae3887873c0db3d67f64bac0f52cb84
David M <david.mosquera.1@ulaval.ca>
2026-01-29 17:39:46 -0500

    win lose conditions done

--- src/main.rs
+++ src/main.rs
@@ -83,6 +83,10 @@ fn main() -> Result<(), Box<dyn Error>> {
             audio.play("move");
         }
 
+        if player.detect_hits(&mut invaders) {
+            audio.play("explode");
+        }
+
         // Draw & render
         let drawables: Vec<&dyn Drawable> = vec![&player, &invaders];
         for drawable in drawables {
@@ -90,6 +94,16 @@ fn main() -> Result<(), Box<dyn Error>> {
         }
         let _ = render_transiver.send(current_frame);
         thread::sleep(Duration::from_millis(1));
+
+        // WIN or LOSE
+        if invaders.all_killed() {
+            audio.play("win");
+            break 'gameloop;
+        }
+        if invaders.reached_bottom() {
+            audio.play("lose");
+            break 'gameloop;
+        }
     }
     
     // wait that the audio threads finish
--- src/player.rs
+++ src/player.rs
@@ -1,6 +1,6 @@
 use std::time::Duration;
 
-use crate::{NUM_COLUMNS, NUM_ROWS, frame::{Drawable, Frame}, shot::Shot};
+use crate::{NUM_COLUMNS, NUM_ROWS, frame::{Drawable, Frame}, invaders::Invaders, shot::Shot};
 
 pub struct Player {
     x: usize,
@@ -45,6 +45,19 @@ impl Player {
         }
 
         self.shots.retain(|shot| !shot.dead()); // cleanup dead shots
+    }
+
+    pub fn detect_hits(&mut self, invaders: &mut Invaders) -> bool {
+        let mut hit_something = false;
+        for shot in self.shots.iter_mut() {
+            if !shot.exploding {
+                if invaders.kill_invader_at(shot.x, shot.y) {
+                    hit_something = true;
+                    shot.explode();
+                }
+            }
+        }
+        hit_something
     }
 }
 
